4ec1ecf3b69c452623799cb27bb1a1fc1bfa54a6
ciyucapa <cindycaceres134@gmail.com>
2021-07-16 12:05:38 -0500

    init

--- react/Greeting.tsx
+++ react/Greeting.tsx
@@ -4,8 +4,8 @@ type Props = {
   name: string
 }
 
-function Greeting({ name }: Props) {
+function trainingApp({ name }: Props) {
   return <div>Hey, {name}</div>
 }
 
-export default Greeting
+export default trainingApp
