eab9dec6bf2a66f6bc20e77651a6ce71303aed45
ciyucapa <cindycaceres134@gmail.com>
2021-07-27 16:47:31 -0500

    cambios apps

---
+++ react/ExpirationPumApp.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+
+import ExpirationAppValidation from './interfaces/ExpirationAppValidation'
+import useExpiration from './hooks/useExpiration'
+import ExpirationInformation from './interfaces/ExpirationInformation'
+
+const CSS_HANDLES = [
+  'container-expiration-pum',
+  'title-expiration',
+  'value-expiration',
+  'title-pum',
+  'value-pum',
+] as const
+
+const InformationExpiration = (props: ExpirationInformation) => {
+  const { handles } = useCssHandles(CSS_HANDLES, props.blockClass)
+
+  return (
+    <div className={`${handles['container-expiration-pum']}`}>
+      <span className={`${handles['title-expiration']}`}>
+        {props.validUntilDate ? 'Vencimiento' : ''}
+      </span>
+      <span className={`${handles['value-expiration']}`}>
+        {props.validUntilDate}
+      </span>
+    </div>
+  )
+}
+
+const InformationPum = (props: ExpirationInformation) => {
+  const { handles } = useCssHandles(CSS_HANDLES, props.blockClass)
+
+  return (
+    <div className={`${handles['container-expiration-pum']}`}>
+      <span className={`${handles['title-pum']}`} />
+      <span className={`${handles['value-pum']}`}>{props.pum}</span>
+    </div>
+  )
+}
+
+const ExpirationApp = (props: ExpirationAppValidation) => {
+  const hook = useExpiration()
+
+  return (
+    <>
+      {props.date ? <InformationExpiration {...hook} /> : ''}
+      {props.pums ? <InformationPum {...hook} /> : ''}
+    </>
+  )
+}
+
+export default ExpirationApp
